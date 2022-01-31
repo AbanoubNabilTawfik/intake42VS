df24d303371e4deb139ed9e7c55e068373e5dc7a
Abanoub Nabil <abanoub.nabil2016@gmail.com>
2022-01-31 10:58:19 +0200

    Add employee class

---
+++ Intake42.tfs/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intake42.tfs
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+    }
+}
